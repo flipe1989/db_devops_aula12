6aaf6b94679603357d15666c3886fbad4907d860
flipe1989 <60761146+flipe1989@users.noreply.github.com>
2020-05-22 20:35:30 -0300

    novo arquivo

--- test_banco.py
+++ test_banco.py
@@ -6,8 +6,8 @@ def banco():
     c = conn.cursor()
 
     c.execute('''CREATE TABLE teste (id integer, nome text not null)''')
-    c.execute("INSERT INTO teste VALUES (1, 'Thiago')")
-    c.execute("INSERT INTO teste VALUES (2, 'Kuma')")
+    c.execute("INSERT INTO teste VALUES (1, 'felipe')")
+    c.execute("INSERT INTO teste VALUES (2, 'dias')")
     c.execute("INSERT INTO teste VALUES (3, 'Impacta')")
 
     c.execute('''SELECT count(*)  FROM  teste''')
